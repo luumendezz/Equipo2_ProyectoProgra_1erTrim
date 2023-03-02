f321e841e674eafa365e3e1c9694bb179ef7012b
Gabriel de Jesús <martinezcgabriel24@gmail.com>
2023-03-01 21:22:05 -0600

    Up Github

---
+++ Proyecto1erTrimestreProgramacion_Equipo2/Form1.cs
@@ -0,0 +1,10 @@
+namespace Proyecto1erTrimestreProgramacion_Equipo2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
