989be7d2354ad5ec5b222ca84fb86f46055e9107
luumendezz <130721970+luumendezz@users.noreply.github.com>
2023-04-28 21:18:08 -0600

    cambios en program.cs
    Luna Méndez Sanabria

--- Proyecto1erTrimestreProgramacion_Equipo2/Program.cs
+++ Proyecto1erTrimestreProgramacion_Equipo2/Program.cs
@@ -17,16 +17,6 @@ namespace Proyecto1erTrimestreProgramacion_Equipo2
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new GUI.Principal());
-
-            /* Probar el DAL desde aquí
-             * Borrar el using DAL; cuando se termine de probar
-            
-            EmpleadoBL bl = new EmpleadoBL();
-            string ced = "119690990";
-            if (bl.BuscarEmpleado(ced,"123") != 0)
-                MessageBox.Show("Sirve");
-            else
-                MessageBox.Show("No sirve");*/
         }
     }
 }
